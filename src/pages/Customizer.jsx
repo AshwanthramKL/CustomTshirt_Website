a7a0985d2dccdb97a850c6a4537c00e416e254ec
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-07-17 16:48:04 +0530

    test commit

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,10 +1,16 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { FadeAnimation, SlideAnimation } from "../config/motion";
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  return <div></div>;
+};
 
-export default Customizer
+export default Customizer;
