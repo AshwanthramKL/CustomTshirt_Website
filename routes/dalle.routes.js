7a1e4789f96765516d125559195cd37719fced7e
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-07-23 11:51:22 +0530

    built route for openai Imagegeneration

--- routes/dalle.routes.js
+++ routes/dalle.routes.js
@@ -12,8 +12,20 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from Dalle route" });
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALLLE route" });
+});
+
+router.route("/").post(async (req, res) => {
+  const { prompt } = req.body;
+
+  const response = await openai.createImage({
+    prompt: { prompt },
+    n: 1,
+    size: "1024x1024",
+    response_format: 'b64_hson'
+  });
+  image_url = response.data.data[0].b64_json;
 });
 
 export default router;
